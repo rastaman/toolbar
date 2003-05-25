29faf3c541d6ea0a1249257dff69ea05172a3fb0
bobtarling <bobtarling@28ef289d-591f-0410-a6a9-9aca74cafd90>
2003-05-25 14:07:33 +0000

    Demo UML creation actions

---
+++ trunk/test/src/org/tigris/toolbar/actions/uml/AssociationAction.java
@@ -0,0 +1,19 @@
+/*
+ * AssociationAction.java
+ *
+ * Created on 11 May 2003, 22:30
+ */
+
+package org.tigris.toolbar.actions.uml;
+
+/**
+ * A simple button action that simply logs that it took place.
+ *
+ * @author Bob Tarling
+ */
+public class AssociationAction extends UmlAction {
+
+    public AssociationAction() {
+        super("Association");
+    }
+}

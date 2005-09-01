0d45bae0ca6534f03aabbac305758de5e29c5da0
nobody <nobody@28ef289d-591f-0410-a6a9-9aca74cafd90>
2005-09-01 21:28:28 +0000

    This commit was manufactured by cvs2svn to create tag 'VERSION_1_1_0'.

---
+++ tags/VERSION_1_1_0/test/src/org/tigris/toolbar/actions/uml/InterfaceAction.java
@@ -0,0 +1,20 @@
+/*
+ * InterfaceAction.java
+ *
+ * Created on 11 May 2003, 22:30
+ */
+
+package org.tigris.toolbar.actions.uml;
+
+/**
+ * A simple button that puts the canvas into accept interface mode
+ * and logs that it took place.
+ *
+ * @author Bob Tarling
+ */
+public class InterfaceAction extends UmlAction {
+
+    public InterfaceAction() {
+        super("Interface");
+    }
+}
